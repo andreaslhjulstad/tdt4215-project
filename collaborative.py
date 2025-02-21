810712ffe999ddbd6e3735875799e93f00dd63ad
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2025-02-21 16:36:29 +0100

    feat: optimize method to calculate recommendations for users
    Previously, the user-article matrix and the user similarity matrix were computed for every user. Now only does it once and the other methods use them as input

--- collaborative.py
+++ collaborative.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import ndarray
 import pandas as pd
 from pandas import DataFrame, Series
 from sklearn.metrics.pairwise import cosine_similarity
@@ -8,15 +9,12 @@ def create_user_article_matrix(df: DataFrame):
     # The article_ids_clicked column has an array of article_ids as value
     # Explode the array so each article_id in the list gets it's own row
     exploded = df.explode("article_ids_clicked")
-
-    # TODO: find better solution
-    clicked_df = exploded[["user_id", "article_ids_clicked"]].copy()
-    clicked_df["clicked"] = 1
+    exploded["clicked"] = 1
 
     # Creates a user-article matrix where the index is the user id and the columns are article IDs
     # The value for each user-article pair is 1 if the user clicked on the article, and 0 otherwise
     user_article_matrix = pd.pivot_table(
-        clicked_df,
+        exploded,
         values="clicked",
         index="user_id",
         columns="article_ids_clicked",
@@ -31,8 +29,8 @@ def create_user_article_matrix(df: DataFrame):
 def calculate_user_similarity(user_article_matrix: DataFrame):
     # Calculate user similarity for pairs (i, j) using cosine similarity
     user_similarity_values = cosine_similarity(user_article_matrix.fillna(0))
-    # Fill diagonal with NaN to ensure that pairs (i, i) are not counted
-    np.fill_diagonal(user_similarity_values, np.nan)
+    # Fill diagonal with 0 to ensure that pairs (i, i) are not counted
+    np.fill_diagonal(user_similarity_values, 0)
 
     user_similarity_df = pd.DataFrame(
         user_similarity_values,
@@ -50,7 +48,7 @@ def create_neighborhood(
     picked_userid: int,
 ):
     neighborhood = user_similarity[
-        user_similarity[picked_userid] > neighborhood_threshold
+        user_similarity.loc[picked_userid] > neighborhood_threshold
     ][picked_userid].sort_values(ascending=False)[:neighborhood_size]
 
     return neighborhood
@@ -88,18 +86,9 @@ def calculate_scores(similar_users_clicked: DataFrame, neighborhood: Series):
 def get_recommendations_for_user(
     userid: int,
     n_recommendations: int,
-    impressions: DataFrame,
-    similarity_threshold: float,
-    neighborhood_size: int,
+    user_article_matrix: DataFrame,
+    neighborhood: DataFrame,
 ):
-    user_article_matrix = create_user_article_matrix(impressions)
-
-    user_similarity = calculate_user_similarity(user_article_matrix)
-
-    neighborhood = create_neighborhood(
-        user_similarity, similarity_threshold, neighborhood_size, userid
-    )
-
     # Limit dataset to articles that similar users have clicked, and only the ones the selected user has not clicked
     userid_clicked = user_article_matrix[user_article_matrix.index == userid].dropna(
         axis=1, how="all"
@@ -114,9 +103,43 @@ def get_recommendations_for_user(
     return scores.head(n_recommendations)
 
 
+def compute_recommendations_for_users(
+    users: ndarray,
+    impressions: DataFrame,
+    n_recommendations: int,
+    similarity_threshold: float,
+    neighborhood_size: int,
+):
+    user_article_matrix = create_user_article_matrix(impressions)
+    user_similarity = calculate_user_similarity(user_article_matrix)
+    recommendations_dict = {}
+    for user in users:
+        neighborhood = create_neighborhood(
+            user_similarity, similarity_threshold, neighborhood_size, user
+        )
+        recommendations = get_recommendations_for_user(
+            user, n_recommendations, user_article_matrix, neighborhood
+        )
+        recommendations_dict[user] = recommendations.index.tolist()
+        print(f"Calculated recommendations for user: {user}")
+    recommendations_df = pd.DataFrame(
+        {
+            "user_id": list(recommendations_dict.keys()),
+            "recommended_article_ids": list(recommendations_dict.values()),
+        }
+    ).set_index("user_id")
+    return recommendations_df
+
+
 def main():
     behavior_data = pd.read_parquet("./data/train/behaviors.parquet")
-    print(get_recommendations_for_user(10701, 10, behavior_data, 0.2, 10))
+
+    user_article_matrix = create_user_article_matrix(behavior_data)
+    user_similarity = calculate_user_similarity(user_article_matrix)
+
+    neighborhood = create_neighborhood(user_similarity, 0.2, 10, 10701)
+
+    print(get_recommendations_for_user(10701, 10, user_article_matrix, neighborhood))
 
 
 if __name__ == "__main__":
