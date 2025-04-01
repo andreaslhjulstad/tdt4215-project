784de606b02af95d7073c4df9891af41418940cf
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2025-04-01 13:25:54 +0200

    feat: finalize matrix factorization

--- matrixfactorization.py
+++ matrixfactorization.py
@@ -1,117 +1,140 @@
 import numpy as np
 import pandas as pd
+import datetime
+
+from utils import create_user_article_matrix
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def create_user_article_matrix(df: pd.DataFrame):
-    # The article_ids_clicked column has an array of article_ids as value
-    # Explode the array so each article_id in the list gets it's own row
-    exploded = df.explode("article_ids_clicked")
-    exploded["clicked"] = 1
+def train_matrix_factorization(
+    interactions: pd.DataFrame,
+    n_factors: int,
+    n_iterations: int,
+    learning_rate: float,
+    regularization: float,
+):
+    """
+    Train a matrix factorization model using stochastic gradient descent.
 
-    # Creates a user-article matrix where the index is the user id and the columns are article IDs
-    # The value for each user-article pair is 1 if the user clicked on the article, and 0 otherwise
-    user_article_matrix = pd.pivot_table(
-        exploded,
-        values="clicked",
-        index="user_id",
-        columns="article_ids_clicked",
-    )
+    Parameters:
+        interactions (pd.DataFrame): DataFrame of user-item interactions.
+        n_factors (int): Number of latent factors.
+        n_iterations (int): Number of iterations for SGD.
+        learning_rate (float): Learning rate for SGD.
+        regularization (float): Regularization parameter.
 
-    sparse_user_article_matrix = user_article_matrix.astype(
-        pd.SparseDtype(float, np.nan)
-    )
-    return sparse_user_article_matrix
+    Returns:
+        tuple: Tuple containing user and item factors.
+    """
+    user_article_matrix = create_user_article_matrix(interactions)
+
+    users = user_article_matrix.index
+    articles = user_article_matrix.columns
+
+    user_factor_map = {}
+    for user in users:
+        user_factors = np.random.normal(scale=1.0 / n_factors, size=(n_factors,))
+        user_factor_map[user] = user_factors
+    item_factor_map = {}
+    for item in articles:
+        item_factors = np.random.normal(scale=1.0 / n_factors, size=(n_factors,))
+        item_factor_map[item] = item_factors
+    for iteration in range(n_iterations):
+        for u in users:
+            for i in articles:
+                user_interaction = user_article_matrix.loc[u, i]
+                if not pd.isna(user_interaction):
+                    user_factor = user_factor_map[u]
+                    item_factor = item_factor_map[i]
+                    pred = sigmoid(
+                        np.dot(user_factor, item_factor)
+                    )  # Use sigmoid function to scale between 0 and 1
+                    error = user_interaction - pred
+                    user_factor_map[u] += learning_rate * (
+                        error * item_factor - regularization * user_factor
+                    )
+                    item_factor_map[i] += learning_rate * (
+                        error * user_factor - regularization * item_factor
+                    )
+        print("Finished iteration", iteration + 1)
+
+    return user_factor_map, item_factor_map
 
 
-class MatrixFactorization:
+def predict(user_factors, item_factors):
+    """
+    Predict the interaction between users and items using the trained user and item factors.
 
-    def __init__(
-        self,
-        n_factors: int,
-        n_iterations: int,
-        learning_rate: float,
-        regularization: float,
-    ):
-        self.n_factors = n_factors
-        self.n_iterations = n_iterations
-        self.learning_rate = learning_rate
-        self.regularization = regularization
+    Parameters:
+        user_factors (dict): Dictionary of user factors.
+        item_factors (dict): Dictionary of item factors.
 
-    def predict(self, interactions: pd.DataFrame):
-        user_article_matrix = create_user_article_matrix(interactions)
-
-        users = user_article_matrix.index
-        articles = user_article_matrix.columns
-
-        user_factor_map = {}
-        for user in users:
-            user_factors = np.random.normal(
-                scale=1.0 / self.n_factors, size=(self.n_factors,)
-            )
-            user_factor_map[user] = user_factors
-        item_factor_map = {}
-        for item in articles:
-            item_factors = np.random.normal(
-                scale=1.0 / self.n_factors, size=(self.n_factors,)
-            )
-            item_factor_map[item] = item_factors
-        for _ in range(self.n_iterations):
-            for u in users:
-                for i in articles:
-                    user_interaction = user_article_matrix.loc[u, i]
-                    if not pd.isna(user_interaction):
-                        user_factor = user_factor_map[u]
-                        item_factor = item_factor_map[i]
-                        pred = sigmoid(
-                            np.dot(user_factor, item_factor)
-                        )  # Use sigmoid function to scale between 0 and 1
-                        error = user_interaction - pred
-                        user_factor_map[u] += self.learning_rate * (
-                            error * item_factor - self.regularization * user_factor
-                        )
-                        item_factor_map[i] += self.learning_rate * (
-                            error * user_factor - self.regularization * item_factor
-                        )
-            print("FINISHED ITERATION", _)
-
-        predictions = pd.DataFrame(
-            index=users,
-            columns=articles,
-            dtype=float,
-        )
-
-        # Fill the prediction matrix
-        for user_id in users:
-            user_factor = user_factor_map[user_id]
-            for article_id in articles:
-                item_factor = item_factor_map[article_id]
-                predictions.loc[user_id, article_id] = sigmoid(
-                    np.dot(user_factor, item_factor)
-                )
-
-        return predictions
+    Returns:
+        pd.DataFrame: DataFrame of predicted interactions.
+    """
+    predictions = pd.DataFrame(index=user_factors.keys(), columns=item_factors.keys())
+    for user, user_factor in user_factors.items():
+        for item, item_factor in item_factors.items():
+            predictions.loc[user, item] = sigmoid(np.dot(user_factor, item_factor))
+    return predictions
 
 
 def compute_recommendations_for_users(
-    users,
+    users: np.ndarray,
     behaviors: pd.DataFrame,
-    n_recommendations,
-    n_factors,
-    n_iterations,
-    learning_rate,
-    regularization,
+    n_recommendations: int,
+    n_factors: int,
+    n_iterations: int,
+    learning_rate: float,
+    regularization: float,
+    time_window: datetime.timedelta = None,
+    current_date: datetime.datetime = None,
 ):
-    filtered_behaviors = behaviors[behaviors.index.isin(users)]
+    """
+    Compute recommendations for a list of users using matrix factorization.
 
-    matrix_factorization = MatrixFactorization(
-        n_factors, n_iterations, learning_rate, regularization
+    Parameters:
+        users (np.ndarray): Array of user IDs for whom to compute recommendations.
+        behaviors (pd.DataFrame): DataFrame of user interactions with articles.
+        n_recommendations (int): Number of recommendations to return for each user.
+        n_factors (int): Number of latent factors for matrix factorization.
+        n_iterations (int): Number of iterations for SGD.
+        learning_rate (float): Learning rate for SGD.
+        regularization (float): Regularization parameter.
+        time_window (datetime.timedelta, optional): Time window for filtering articles.
+        current_date (datetime.datetime, optional): Current date for filtering articles.
+
+    Returns:
+        pd.DataFrame: DataFrame of recommended articles for each user.
+    """
+    if current_date is not None and time_window is not None:
+        # Only load articles if date filtering is needed
+        articles = pd.read_parquet("data/articles.parquet").set_index("article_id")
+        articles = articles[articles["published_time"] > current_date - time_window]
+        # Filter behaviors by users and articles
+        filtered_behaviors = (
+            behaviors[behaviors.index.isin(users)]
+            .explode("article_ids_clicked")
+            .loc[lambda df: df["article_ids_clicked"].isin(articles.index)]
+        )
+    else:
+        # Just filter by users if no date filtering needed
+        filtered_behaviors = behaviors[behaviors.index.isin(users)].explode(
+            "article_ids_clicked"
+        )
+
+    user_factors, item_factors = train_matrix_factorization(
+        filtered_behaviors,
+        n_factors,
+        n_iterations,
+        learning_rate,
+        regularization,
     )
 
-    predictions = matrix_factorization.predict(filtered_behaviors)
+    predictions = predict(user_factors, item_factors)
 
     recommendations_dict = {}
 
@@ -119,7 +142,7 @@ def compute_recommendations_for_users(
     for u in predictions.index:
         predicted_values = []
         for i in article_ids:
-            predicted_values.append(predictions.loc[u][i])
+            predicted_values.append(predictions.loc[u, i])
         sorted_article_ids = [x for x, _ in sorted(zip(article_ids, predicted_values))]
         recommendations_dict[u] = sorted_article_ids[:n_recommendations]
 
@@ -134,11 +157,13 @@ def compute_recommendations_for_users(
 
 def main():
     behaviors = pd.read_parquet("./data/train/behaviors.parquet")
-    n_users = 100
+    n_users = 1000
 
-    matrix_factorization = MatrixFactorization(3, 10, 0.01, 0.1)
+    user_factors, item_factors = train_matrix_factorization(
+        behaviors.head(n_users), 3, 10, 0.01, 0.1
+    )
 
-    predictions = matrix_factorization.predict(behaviors.head(n_users))
+    predictions = predict(user_factors, item_factors)
 
     print(predictions)
 
